01f254b11b06d3f43d7c5e9bc921c76ff519ddfb
AshvinJoseph2 <ashvinj893@gmail.com>
2025-12-17 10:45:16 +0530

    ADD : home page project controller

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const bookController = require('../controllers/bookController')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
+const multerMiddleware = require('../middlewares/multerMiddleware')
 
 const router = new express.Router()
 
 // register
 router.post('/register',userController.registerController)
-
 // Login
 router.post('/login',userController.loginController)
+// google login
+router.post('/google-login',userController.googleLoginController)
+// home project
+router.get('/home/books',bookController.getHomeBookController)
 
+// -------------------------Authorised user------------------------
+// -------------------------Role : User------------------------
+
+// add book - request body in formdata,header should has token
+router.post('/user/add/book',jwtMiddleware,multerMiddleware.array('uploadImg'),bookController.addBookController)
 
 module.exports = router
